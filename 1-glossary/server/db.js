d49b6b704c3d60b7ca05153bc04af13557427847
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-08 16:17:29 -0800

    Connect to mongo data base and create schema/model

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,19 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+console.log(process.env.PORT, process.env.DB_NAME);
+mongoose.connect(`mongodb://127.0.0.1:${process.env.PORT}/${process.env.DB_NAME}`);
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+const glossarySchema = new mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+const Word = mongoose.model('Word', glossarySchema);
+
+module.exports = Word;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
+const axios = require('axios');
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+
+//get all
+axios.get('/getAll', (req, res) => {
+  console.log('testing from getall');
+})
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
+// console.log(process.env);

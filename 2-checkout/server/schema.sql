d8f2072709be28e661e367b0ee3970f5003e24b7
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-10 12:10:06 -0800

    Create data base and tables

--- 2-checkout/server/schema.sql
+++ 2-checkout/server/schema.sql
@@ -2,30 +2,32 @@ CREATE DATABASE checkout;
 
 USE checkout;
 
-CREATE TABLE user (
+CREATE TABLE cards (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  num INT,
+  cvv INT NOT NULL,
+  expiration INT NOT NULL
+);
+
+CREATE TABLE locations (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  line1 VARCHAR(50) NOT NULL,
+  line2 VARCHAR(50) DEFAULT '',
+  city TEXT NOT NULL,
+  currentState TEXT NOT NULL,
+  zipcode INT NOT NULL
+);
+
+CREATE TABLE users (
   cookie VARCHAR(50) NOT NULL PRIMARY KEY,
   username VARCHAR(20) NOT NULL,
   email VARCHAR(50) NOT NULL,
   pw VARCHAR(18) NOT NULL,
   addressID INT NOT NULL,
   cardNumber INT NOT NULL,
-  FOREIGN KEY(cardID) REFERENCES card(num),
-  FOREIGN KEY(addressID) REFERENCES location(id)
-)
+  FOREIGN KEY(cardNumber) REFERENCES cards(id),
+  FOREIGN KEY(addressID) REFERENCES locations(id)
+);
 
-CREATE TABLE card (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  num INT NOT NULL,
-  CVV INT NOT NULL,
-  expiration INT NOT NULL
-)
 
-CREATE TABLE location (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  line1 VARCHAR(50) NOT NULL,
-  line2 VARCHAR(50) DEFAULT '',
-  city TEXT NOT NULL,
-  currentState TEXT NOT NULL,
-  zipcode INT NOT NULL,
-)
 

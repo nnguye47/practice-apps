0f0a08d5b64f255379895ebb5811a904b1b9c6fe
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-11 09:58:57 -0800

    Implement a form in create account F1 page

--- 2-checkout/client/src/F1.jsx
+++ 2-checkout/client/src/F1.jsx
@@ -1,16 +1,38 @@
 import React, {useState} from 'react';
 import { render } from "react-dom";
-import F2 from './F2.jsx'
+import F2 from './F2.jsx';
+import axios from 'axios';
 
 const F1 = () => {
+  const [userName, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   const renderF2 = (e) => {
     e.preventDefault();
-    render(<F2 />, document.getElementById("root"));
-  }
+    var account = {username: userName, email: email, password: password};
+    console.log(account);
+    // render(<F2 />, document.getElementById("root"));
+  };
 
   return (
     <div>
-      hello from F1
+        <h1>hello from F1</h1>
+      <input type='text' onChange={handleUsername} value={userName} placeholder='username...'></input><br></br>
+      <input type='email' onChange={handleEmail} value={email} placeholder='email...'></input><br></br>
+      <input type='password' onChange={handlePassword} value={password} placeholder='password...'></input><br></br>
       <button onClick={renderF2}>Proceed</button>
     </div>
   )
